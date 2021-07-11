dbc5abd6ce7cbab935d123cb40c98b5e5ba2407c
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-07-11 22:21:53 +0530

    started learning dsa in cpp. done with linkedlist

---
+++ linklist/single_linked_list.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+  int data;
+  node *next;
+};
+
+node *head;
+
+void create_node(node *ptr)
+{
+  int data;
+  // cout<<" Enter a data : ";
+  cin >> data;
+  ptr->data = data;
+
+  node *temp = head;
+
+  if (head == 0)
+  {
+    head = ptr;
+  }
+  else
+  {
+    while (temp->next != 0)
+    {
+      temp = temp->next;
+    }
+    temp->next = ptr;
+  }
+  ptr->next = 0;
+}
+
+void showdata()
+{
+  node *temp = head;
+  if (head == 0)
+  {
+    cout << "\n linked list is empty..";
+    return;
+  }
+  while (temp->next != 0)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+  cout << temp->data;
+}
+
+int main()
+{
+  node fst, snd;
+  // create_node(&fst);
+  // create_node(&snd);
+  // cout<<fst.data<<" "<<fst.next<<endl;
+  // cout<<snd.data<<" "<<snd.next;
+  showdata();
+  return 0;
+}
