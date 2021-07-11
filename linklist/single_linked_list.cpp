1d36b214165e9d0f417b7e89b597629e6162b2cb
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-07-11 23:11:04 +0530

    added some push operation

--- linklist/single_linked_list.cpp
+++ linklist/single_linked_list.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ******* (start)  initializing new data type node ******
 struct node
 {
   int data;
   node *next;
 };
+// ******* (ends) initializing new data type node  ******
 
-node *head;
+node *head; // initializing head pinter of linkedlist
+
+// ****** (start) ( PUSH OPERATION ) fuction for adding node at the end of linkedlist (strt here) *********
 
 void create_node(node *ptr)
 {
@@ -33,6 +37,61 @@ void create_node(node *ptr)
   ptr->next = 0;
 }
 
+void create_node_at_start(node *ptr)
+{
+  node *temp = ptr;
+  int data;
+  cin >> data;
+  ptr->data = data;
+
+  if (head == 0)
+  {
+    head = ptr;
+    head->next = 0;
+  }
+  else
+  {
+    ptr->next = head;
+    head = ptr;
+  }
+}
+
+void create_node_after_value(node *ptr, int value)
+{
+  node *current = head;
+  node *previous = head;
+  cout << current << " " << previous << endl;
+  int data_to_insert;
+  cin >> data_to_insert;
+  ptr->data = data_to_insert;
+  // cout<<->data<<endl;
+
+  cout << previous->data << " ";
+  if (head == 0)
+  {
+    cout << " no node is there so creating at the start. ";
+    head = ptr;
+    head->next = 0;
+  }
+  else
+  {
+    while (current->data != value)
+    {
+      previous = current;
+      current = current->next;
+    }
+    previous = current;
+    current = current->next;
+    previous->next = ptr;
+    ptr->next = current;
+  }
+  cout << "end " << endl;
+}
+
+// ****** (end) fuction for adding node at the end of linkedlist (ends here) *********
+
+// ********* (start) fuction for showing all the data in linkedlist.(strt here ) *******
+
 void showdata()
 {
   node *temp = head;
@@ -49,13 +108,24 @@ void showdata()
   cout << temp->data;
 }
 
+// ********* (ends) fuction for showing all the data in linkedlist. ends here *******
+
+// ********** (start) execution of program starts here *********
+
 int main()
 {
-  node fst, snd;
+  node fst, snd, trd, fth, fifth, sth, seth;
   create_node(&fst);
   create_node(&snd);
-  cout<<fst.data<<" "<<fst.next<<endl;
-  cout<<snd.data<<" "<<snd.next;
+  create_node(&trd);
+  create_node(&fth);
+  create_node(&fifth);
+  create_node(&sth);
+  showdata();
+  cout << endl;
+  create_node_after_value(&seth, 10);
+  // cout<<fst.data<<" "<<fst.next<<endl;
+  // cout<<snd.data<<" "<<snd.next;
   showdata();
   return 0;
 }
