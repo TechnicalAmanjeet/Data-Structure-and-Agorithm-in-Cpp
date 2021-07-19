1aa6d38d316630bfe7bb4c21ed93b8bd1694c3cc
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-07-20 00:42:34 +0530

    done with insertion sort algorithm in sorting series

---
+++ Sorting_Algorithm/insertion_sort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[],int size_arr){
+    // int size_arr = sizeof(*arr)/sizeof(arr[0]);
+    // for(int k=0;k<size_arr;++k){cout<<arr[k]<<" ";}
+    // int a[] = arr[size_arr];
+    for(int j=1;j<size_arr;++j){
+        int i = j-1;
+        if(arr[j]<arr[i]){
+            swap(arr[i],arr[j]);
+            --i;
+            while(i>=0){
+                if(arr[i+1]<arr[i]){swap(arr[i+1],arr[i]);--i;}
+                else{break;}
+            }           
+        }
+        cout<<j<<" : ";
+        for(int k=0;k<size_arr;++k){cout<<arr[k]<<" ";}
+        cout<<endl;
+    }
+}
+
+int main(){
+    int arr[]={9,8,7,5,3,4,6,1,2};
+    int size_arr = sizeof(arr)/sizeof(arr[0]);
+    cout<<"0 : ";
+    for(int i=0;i<size_arr;++i){cout<<arr[i]<<" ";}
+    cout<<endl;
+    insertion_sort(arr , size_arr);
+    // for(int j=1;j<size_arr;++j){
+    //     int i = j-1;
+    //     if(arr[j]<arr[i]){
+    //         swap(arr[i],arr[j]);
+    //         --i;
+    //         while(i>=0){
+    //             if(arr[i+1]<arr[i]){swap(arr[i+1],arr[i]);--i;}
+    //             else{break;}
+    //         }           
+    //     }
+    //     cout<<j<<" : ";
+    //     for(int k=0;k<size_arr;++k){cout<<arr[k]<<" ";}
+    //     cout<<endl;
+    // }
+    return 0;
+}
