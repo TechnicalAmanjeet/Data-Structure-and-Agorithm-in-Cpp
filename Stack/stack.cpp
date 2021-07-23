194bf42659852b7595ce789d53360ecc0b2b6824
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-07-23 10:37:30 +0530

    Added some new functionality to stack

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -18,7 +18,17 @@ public:
     void peak(int position);
     void display();
     void showfull();
+    void changevalue();
+    void totalElement();
 };
+
+void stack_main::changevalue(){
+    cout<<"I have to implement it "<<endl;
+}
+
+void stack_main::totalElement(){
+    cout<<"Total Emlement is Stack : "<<top+1<<endl;
+}
 
 stack_main::stack_main(){
         full = 5;
@@ -34,7 +44,7 @@ bool stack_main :: isfull()
     {
         if (top == full-1 )
         {
-            cout << "\n Stack is full. " << endl;
+            cout << "\n Stack is full. Overflow. " << endl;
             return 1;
         }
         // cout<<"full : "<<full;
@@ -45,7 +55,7 @@ bool stack_main::isempty()
     {
         if (top == -1)
         {
-            cout << "\n Stack is empty" << endl;
+            cout << "\n Stack is empty i.e Underflow" << endl;
             return 1;
         }
         return 0;
@@ -63,7 +73,7 @@ void stack_main :: push(int data){
 
 void stack_main :: push(){
         if(isfull()){
-            cout<<"\n Stark is full"<<endl;
+            cout<<"\n Stark is full overflow"<<endl;
         }
         else{
             ++top;
@@ -82,7 +92,7 @@ void stack_main :: peak(int position){
 
 void stack_main :: pop(){
         if(isempty()){
-            cout<<"\n Stack is empty"<<endl;
+            cout<<"\n Stack is empty Underflow"<<endl;
         }
         else{
             --top;
@@ -93,7 +103,7 @@ void stack_main :: pop(){
 
 void stack_main::display(){
     if(top==-1){
-        cout<<"Stack is empty";
+        cout<<"Stack is empty Underflow";
     }
     else{
         for(int i=0;i<=top;++i){
@@ -103,19 +113,74 @@ void stack_main::display(){
 }
 
 int main(){
-    stack_main s;
-    // cout<<s.isempty();
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
-    s.push(5);
+    stack_main stack;
+    while(1){
+        cout<<"  \n     ************** Stack in Cpp ************** \n press 0. Exit. \n Press 1. Push value without data. \n Press 2. PUsh value without data. \n Press 3. Pop Element from stack. \n Press 4. isfull operation \n Press 5. isEmpty operation \n Press 6. peak opearation  \n Press 7. changevalue operation \n Press 8. Display oepration.   "<<endl;
 
-    s.display();
-    cout<<endl;
-    s.pop();
-    s.pop();
-    s.display();
-    cout<<"\n";
-    s.showfull();
+        int choice;
+        cout<<endl;
+        cout<<" Enter you Choice : ";
+        cin>>choice;
+        cout<<endl;
+        if(choice==0) break;
+        switch (choice)
+        {
+        case 1:
+            cout<<" \n Your choice is PUsh without value : "<<endl;
+            if(stack.isfull()){
+                cout<<" Stack is in overflow you cannot add value!!"<<endl;
+            }
+            else stack.push();
+            break;
+        case 2: 
+            cout<<"\n YOur choice is push with value : "<<endl;
+            int data;
+            if(stack.isfull()){
+                cout<<" Stack is in overflow you cannot add value!!"<<endl;
+            }
+            else{
+                cout<<"Enter value to push in stack : ";
+                cin>>data;
+                cout<<endl;
+                stack.push(data);
+            }
+            break;
+
+        case 3: 
+            if(stack.isempty()) cout<<"Underflow you can't delete value "<<endl;
+            else stack.pop();
+            break;
+
+        case 4:
+            stack.isfull();
+            break;
+        
+        case 5:
+            stack.isempty();
+            break;
+
+        case 6:
+            if(stack.isempty()) cout<<"underflow condition!"<<endl;
+            else{
+                int value;
+                cout<<" Enter postion to peak value from stack : ";
+                cin>>value;
+                stack.peak(value);
+            }
+            break;
+        
+        case 7:
+            stack.changevalue();
+            break;
+        
+        case 8:
+            stack.display();
+            break;
+
+
+        default:
+            cout<<"YOu Entered wrong choice!!"<<endl;
+            break;
+        }
+    }
 }
