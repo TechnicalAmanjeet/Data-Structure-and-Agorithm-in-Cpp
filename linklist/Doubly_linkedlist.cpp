be83256a88c84ca2124762ba9cb8c22f182f3a19
Amanjeet Kumar <techamanjeet1@gmail.com>
2021-08-02 23:04:28 +0530

    approximatly done with the doubly linked list

--- linklist/Doubly_linkedlist.cpp
+++ linklist/Doubly_linkedlist.cpp
@@ -64,6 +64,7 @@ void dublin::displayReverse(){
 void dublin::push(){
     int data;
     Node* new_node = new Node();
+    ++count;
     cout<<"Enter data : ";
     cin>>data;
     new_node->data = data;
@@ -81,10 +82,156 @@ void dublin::push(){
     displayReverse();
 }
 
+void dublin::insert(){
+    int data;
+    Node* new_node = new Node();
+    ++count;
+    cout<<"Enter data : ";
+    cin>>data;
+    new_node->data = data;
+    if(isempty()){
+        Start = new_node;
+        new_node->prev = new_node;
+        Start->next = NULL;
+        End=new_node;
+    }
+    else{
+        Start->prev = new_node;
+        new_node->next = Start;
+        new_node->prev = new_node;
+        Start = new_node;
+    }
+    display();
+    // displayReverse();
+}
+
+void dublin::insert(int n){
+    int data;
+    Node* new_node = new Node();
+    ++count;
+    cout<<"Enter data : ";
+    cin>>data;
+    new_node->data = data;
+    if(isempty()){
+        Start = new_node;
+        new_node->prev = new_node;
+        Start->next = NULL;
+        End=new_node;
+    }
+    else if(n>count) {
+        cout<<"There are only "<<count<<" member in list"<<endl;
+        return;
+    }
+    else if(n==1){
+        Start->prev = new_node;
+        new_node->next = Start;
+        new_node->prev = new_node;
+        Start = new_node;
+    }
+    else{
+        Node* temp= Start;
+        --n;
+        --n;
+        while(n--) temp=temp->next;
+        new_node->prev = temp;
+        new_node->next = temp->next;
+        temp->next = new_node;
+        new_node->next->prev = new_node;
+    }
+    display();
+    displayReverse();
+}
+
+void dublin::pop(){
+    if(isempty()) cout<<"List is empty Nothing to delete"<<endl;
+    else if(count==1){
+        Node*  temp=End;
+        Start = NULL;
+        End = NULL;
+        delete(temp);
+        --count;
+    }
+    else{
+        Node* temp = End;
+        temp->prev->next = NULL;
+        End = temp->prev;
+        delete(temp);
+        --count;
+    }
+    display();
+}
+
+void dublin::remove(){
+    if(isempty()){
+        cout<<"List is empty NOthing to Delete"<<endl;
+        return;
+    }
+    else{
+        Node* temp=Start;
+        if(count==1){
+            Start=NULL;
+            End=NULL;
+        }
+        else{
+            temp->next->prev = temp->next;
+            Start = temp->next;
+        }
+        delete(temp);
+        --count;
+        display();
+    }
+}
+
+void dublin::remove(int n){
+    if(isempty()){
+        cout<<"List is empty NOthing to Delete"<<endl;
+        return;
+    }
+    else if(n==count){
+        pop();
+    }
+    else if(n==1){
+        remove();
+    }
+    else{
+        Node* temp=Start;
+        if(count==1){
+            Start=NULL;
+            End=NULL;
+        }
+        else{
+            --n;
+            while(n--) temp=temp->next;
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+        }
+        delete(temp);
+        --count;
+        display();
+    }
+}
+
+void dublin::change(int n,int data){
+    if(n>count){
+        cout<<"There are only "<<count<<" member in list"<<endl;
+        return;
+    }
+    else{
+        Node* temp= Start;
+        --n;
+        while(n--) temp=temp->next;
+        temp->data = data;
+    }
+}
+
 int main(){
     dublin d;
     d.display();
     int t=5;
-    while(t--) d.push();
+    while(t--) d.insert();
+    d.remove(3);
+    // d.display();
+    d.remove(1);
+    d.remove(3);
     d.display();
 }
